4e016f9d8a388b40bb9505affdd7048cc46a12ad
Abhishek Gola <135051591+gola-abhishek@users.noreply.github.com>
2025-05-25 12:36:44 +0530

    sum-2d-matrix

---
+++ 2D Arrays/matricesSum.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class matricesSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter data for matrix 1 : ");
+        System.out.println("Enter no of rows and columns : ");
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+
+        int[][] arr1 = new int[r1][c1];
+        System.out.println("Enter " + r1 * c1 + " elements : ");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter data for matrix 2 : ");
+
+        System.out.println("Enter no of rows and columns : ");
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+
+        int[][] arr2 = new int[r2][c2];
+
+        System.out.println("Enter " + r2 * c2 + " elements : ");
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Matrix 1 : ");
+        print2DArray(arr1);
+        System.out.println("Matrix 2 : ");
+        print2DArray(arr2);
+
+        addMatrices(arr1, r1, c1, arr2, r2, c2);
+    }
+
+    static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void addMatrices(int[][] a, int r1, int c1, int[][] b, int r2, int c2) {
+        if (r1 != r2 && c1 != c2) {
+            System.out.println("Wrong Input - Add Not Possible ");
+            return;
+        }
+
+        int[][] sum = new int[r1][c1];
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        System.out.println("Sum of matrix 1 and matrix 2 : ");
+        print2DArray(sum);
+    }
+}
